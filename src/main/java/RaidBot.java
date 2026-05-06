ec715a5e9ad821bc0aa70e4eee8eaf3a735e22c0
vibesasura-collab <vibesasura@gmail.com>
2026-05-07 00:37:38 +0530

    Update RaidBot.java

--- src/main/java/RaidBot.java
+++ src/main/java/RaidBot.java
@@ -33,13 +33,13 @@ public class RaidBot {
             driver.get("https://elem.cards/guild/raids/");
             sleep(2000);
 
-            driver.get("https://elem.cards/guild/raids/dragon_air/join/");
+            driver.get("https://elem.cards/guild/raids/dragon_fire2/join/");
             sleep(3000);
 
             System.out.println("Joined raid");
 
             // ---------------- RAID PAGE ----------------
-            driver.get("https://elem.cards/guild/raids/dragon_air/");
+            driver.get("https://elem.cards/guild/raids/dragon_fire2/");
 
             int tries = 0;
             boolean started = false;
